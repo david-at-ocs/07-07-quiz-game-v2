6b5123f2f74b209691a6629580b6ccfd31108fc8
David Gardetti <dgardetti@gmail.com>
2015-07-07 17:40:04 -0500

    Quiz V2, part 1 done

--- global.js
+++ global.js
@@ -1,5 +1,14 @@
-var question1 = {question: "How many words are in this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 4", answer: "d"};
+var question1 = {question: "How many words are in this sentence?", possibleAnswers: "a.1\rb. 55\rc. 345\rd. 7\re. 4", answer: "d"};
 var question2 = {question: "What about this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 4", answer: "e"};
 var question3 = {question: "And this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 3", answer: "e"};
 var question4 = {question: "What's your favorite letter.", possibleAnswers: "a. b. c. d. e.", answer: "b"};
 var question5 = {question: "Is there a correct answer to this question?", possibleAnswers: "a. Yes b. No c. Not Sure d. Maybe", answer: "a"}
+
+var quest;
+var choice;
+
+quest = document.getElementById("question");
+choice = document.getElementById("choices");
+
+quest.innerText = question1.question;
+choice.innerText = question1.possibleAnswers;
