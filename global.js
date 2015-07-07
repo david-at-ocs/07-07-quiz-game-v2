7647e08af2b1c7a6cc53910f384375aed6b881a4
David Gardetti <dgardetti@gmail.com>
2015-07-07 15:44:48 -0500

    added global.js with question objects

---
+++ global.js
@@ -0,0 +1,5 @@
+var question1 = {question: "How many words are in this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 4", answer: "d"};
+var question2 = {question: "What about this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 4", answer: "e"};
+var question3 = {question: "And this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 3", answer: "e"};
+var question4 = {question: "What's your favorite letter.", possibleAnswers: "a. b. c. d. e.", answer: "b"};
+var question5 = {question: "Is there a correct answer to this question?", possibleAnswers: "a. Yes b. No c. Not Sure d. Maybe", answer: "a"}
